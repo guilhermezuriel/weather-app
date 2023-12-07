b03f20126e41006f31625242dda08e156ce67fd2
Guilherme Zuriel <guizuriel.dev@gmail.com>
2023-12-07 19:51:01 -0300

    Primeiros estudos e familiarizações

---
+++ app.js
@@ -0,0 +1,13 @@
+//HTTP Requests
+const request = require('postman-request');
+
+/* Request Example
+request('https://www.google.com', function (error, response, body) {
+  const responseobj = {
+    error: error,
+    statusCode: `${response} : ${response.statusCode}`,
+    body: body,
+  };
+  console.log(responseobj);
+});
+*/
